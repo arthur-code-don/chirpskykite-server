2679880ddd185e43c9d93c7367a7755915c8e503
code-don <arthurkoton@proton.me>
2023-12-23 03:59:27 -0500

    debuggin-server

--- index.js
+++ index.js
@@ -33,7 +33,16 @@ app.use(bodyParser.urlencoded({ limit: "100mb", extended: true }));
 const allowedOrigins = ["https://chirpsky.net", "https://chirpskykite-frontend.onrender.com"];
 app.use(cors({
   origin: "*",
+  credentials: true,
 }));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
