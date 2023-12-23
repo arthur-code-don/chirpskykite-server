ad13fcd8bf2c50d33d7072b0c7913d1c9f812db7
code-don <arthurkoton@proton.me>
2023-12-23 03:34:10 -0500

    debuggin-server

--- index.js
+++ index.js
@@ -30,19 +30,10 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "100mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "100mb", extended: true }));
 
-const allowedOrigins = ["https://chirpsky.net"];
+const allowedOrigins = ["https://chirpsky.net", "https://chirpskykite-frontend.onrender.com"];
 app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = "The CORS policy for this site does not allow access from the specified origin.";
-      return callback(new Error(msg), false);
-    }
-
-    return callback(null, true);
-  },
+  origin: "https://chirpsky.net",
+  credentials: true
 }));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
